7c518e6758c7355e4838250441f152ddac70e7fb
Malik FASSI <malikfassifihri@gmail.com>
2023-01-29 23:10:29 +0100

    make amount str

--- gen_data.py
+++ gen_data.py
@@ -6,6 +6,7 @@ import requests
 snapshot_file = "../stakedrop/snapshot-29-01-2023.csv"
 node = "https://osmosis-mainnet-rpc.allthatnode.com:1317"
 
+'''WIP why is it not working ?'''
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, float):
@@ -20,14 +21,14 @@ def get_tx_by_hash(hash: str):
    )
    return resp.json()
 
-tx_resp = get_tx_by_hash("CC83A820624117616E4D4C88D7EAEF84E522EF38AC1EA374552216AA296B09EE")
+tx_resp = get_tx_by_hash("1A20E4A1F89D9DC6D8F5CAAAEA84AF5CE5E130493AFF789462AF5E8B13E00A78")
 
 messages = tx_resp['tx']['body']['messages']
 total = sum(int(msg['amount'][0]['amount']) for msg in messages)
 data = []
 for msg in messages:
      amount = int(msg['amount'][0]['amount'])
-     tx = {'address': msg['to_address'], 'share': "{:.16f}".format(amount/total), 'amount': amount}
+     tx = {'address': msg['to_address'], 'share': "{:.16f}".format(amount/total), 'amount': str(amount)}
      data.append(tx)
 
 print(json.dumps(data, cls=CustomEncoder))
